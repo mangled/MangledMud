e2e1bcb0455974034974a9d1dd2fa0b22fa566dd
mangled <matthew@mangled.me>
2010-12-05 21:46:50 +0000

    finished utils tests

--- ruby/ext/utilswrap.c
+++ ruby/ext/utilswrap.c
@@ -5,7 +5,41 @@
 
 static VALUE utils_class;
 
+static VALUE do_remove_first(VALUE self, VALUE first, VALUE what)
+{
+    (void) self;
+    dbref first_ref = FIX2INT(first);
+    dbref what_ref = FIX2INT(what);
+    return INT2FIX(remove_first(first_ref, what_ref));
+}
+
+static VALUE do_member(VALUE self, VALUE thing, VALUE list)
+{
+    (void) self;
+    dbref thing_ref = FIX2INT(thing);
+    dbref list_ref = FIX2INT(list);
+    return INT2FIX(member(thing_ref, list_ref));
+}
+
+static VALUE do_reverse(VALUE self, VALUE list)
+{
+    (void) self;
+    dbref list_ref = FIX2INT(list);
+    return INT2FIX(reverse(list_ref));
+}
+
+static VALUE do_getname(VALUE self, VALUE loc)
+{
+    (void) self;
+    dbref loc_ref = FIX2INT(loc);
+    return rb_str_new2(getname(loc_ref));
+}
+
 void Init_utils()
 {   
     utils_class = rb_define_class_under(tinymud_module, "Utils", rb_cObject);
+    rb_define_method(utils_class, "remove_first", do_remove_first, 2);
+    rb_define_method(utils_class, "member", do_member, 2);
+    rb_define_method(utils_class, "reverse", do_reverse, 1);
+    rb_define_method(utils_class, "getname", do_getname, 1);
 }
--- ruby/test/utils.rb
+++ ruby/test/utils.rb
@@ -15,8 +15,129 @@ module TinyMud
 			@db.free()
 		end
 		
-		def test_create
+		def test_remove_first
+			thing1 = @db.add_new_record
+			thing2 = @db.add_new_record
+			thing3 = @db.add_new_record
+			record(thing1) {|r| r.merge!({ :next => thing2 }) }
+			record(thing2) {|r| r.merge!({ :next => thing3 }) }
+
 			utils = Utils.new
+			# Not in the list - returns the first!
+			assert_equal(thing1, utils.remove_first(thing1, 100))
+			# First in the list then it skips it
+			assert_equal(thing2, utils.remove_first(thing1, thing1))
+			# Not first so it's removed from the chain
+			assert_equal(thing1, utils.remove_first(thing1, thing2))
+			assert(thing3, @db.get(thing1).next)
+		end
+		
+		def test_member
+			thing1 = @db.add_new_record
+			thing2 = @db.add_new_record
+			thing3 = @db.add_new_record
+			record(thing1) {|r| r.merge!({ :next => thing2 }) }
+			record(thing2) {|r| r.merge!({ :next => thing3 }) }
+
+			utils = Utils.new
+			assert_equal(0, utils.member(thing3 + 1, thing1))
+			assert_equal(1, utils.member(thing1, thing1))
+			assert_equal(1, utils.member(thing2, thing1))
+			assert_equal(1, utils.member(thing3, thing1))
+			assert_equal(1, utils.member(thing3, thing2))
+		end
+		
+		def test_reverse
+			thing1 = @db.add_new_record
+			thing2 = @db.add_new_record
+			thing3 = @db.add_new_record
+			record(thing1) {|r| r.merge!({ :name => "thing1", :next => thing2 }) }
+			record(thing2) {|r| r.merge!({ :name => "thing2", :next => thing3 }) }
+			record(thing3) {|r| r.merge!({ :name => "thing3", :next => NOTHING }) }
+			assert_equal("thing1", @db.get(0).name)
+			assert_equal(thing2, @db.get(0).next)
+			assert_equal(thing3, @db.get(0).next.next)
+
+			utils = Utils.new
+			reversed = utils.reverse(thing1)
+			assert_equal(NOTHING, @db.get(thing1).next)
+			assert_equal(thing1, @db.get(thing2).next)
+			assert_equal(thing2, @db.get(thing3).next)
+			assert_equal("thing3", @db.get(reversed).name)
+			assert_equal(thing2, @db.get(reversed).next)
+			assert_equal(thing1, @db.get(@db.get(reversed).next).next)
+		end
+		
+		def test_getname
+			thing1 = @db.add_new_record
+			record(thing1) {|r| r.merge!({ :name => "cheese", :location => NOTHING }) }
+			utils = Utils.new
+			assert_equal("***NOTHING***", utils.getname(NOTHING))
+			assert_equal("***HOME***", utils.getname(HOME))
+			assert_equal("cheese", utils.getname(thing1))
+		end
+
+		# MOVE THIS SOMEWHERE - DRY
+		def record(i)
+			record = @db.get(i)
+
+			args = {}
+			args[:name] = record.name
+			args[:description] = record.description
+			args[:location] = record.location
+			args[:contents] = record.contents
+			args[:exits] = record.exits
+			args[:next] = record.next
+			args[:key] = record.key
+			args[:fail] = record.fail
+			args[:succ] = record.succ
+			args[:ofail] = record.ofail
+			args[:osucc] = record.osucc
+			args[:owner] = record.owner
+			args[:pennies] = record.pennies
+			args[:flags] = record.flags
+			args[:password] = record.password
+
+			yield args
+
+			args.each do |key, value|
+				case key
+				when :name
+					record.name = value
+				when :description
+					record.description = value
+				when :location
+					record.location = value
+				when :contents
+					record.contents = value
+				when :exits
+					record.exits = value
+				when :next
+					record.next = value
+				when :key
+					record.key = value
+				when :fail
+					record.fail = value
+				when :succ
+					record.succ = value
+				when :ofail
+					record.ofail = value
+				when :osucc
+					record.osucc = value
+				when :owner
+					record.owner = value
+				when :pennies
+					record.pennies = value
+				when :flags
+					record.flags = value
+				when :password
+					record.password = value
+				else
+					raise("Record - unknown key #{key} with #{value}")
+				end
+			end
+
+			@db.put(i, record)
 		end
     end
 end
