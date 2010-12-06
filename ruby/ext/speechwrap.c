ec1bdbc84c8fa486b6081322d1ab3acdec49c80c
mangled <matthew@mangled.me>
2010-12-06 21:25:49 +0000

    speech under test

--- ruby/ext/speechwrap.c
+++ ruby/ext/speechwrap.c
@@ -5,7 +5,117 @@
 
 static VALUE speech_class;
 
+static VALUE do_reconstruct_message(VALUE self, VALUE arg1, VALUE arg2)
+{
+    (void) self;
+    const char* arg1_s = 0;
+    const char* arg2_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    if (arg2 != Qnil) {
+        arg2_s = STR2CSTR(arg2);
+    }
+    return rb_str_new2(reconstruct_message(arg1_s, arg2_s));
+}
+
+static VALUE do_do_say(VALUE self, VALUE player, VALUE arg1, VALUE arg2)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    const char* arg1_s = 0;
+    const char* arg2_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    if (arg2 != Qnil) {
+        arg2_s = STR2CSTR(arg2);
+    }
+    do_say(player_ref, arg1_s, arg2_s);
+    return Qnil;
+}
+
+static VALUE do_do_pose(VALUE self, VALUE player, VALUE arg1, VALUE arg2)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    const char* arg1_s = 0;
+    const char* arg2_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    if (arg2 != Qnil) {
+        arg2_s = STR2CSTR(arg2);
+    }
+    do_pose(player_ref, arg1_s, arg2_s);
+    return Qnil;
+}
+
+static VALUE do_do_wall(VALUE self, VALUE player, VALUE arg1, VALUE arg2)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    const char* arg1_s = 0;
+    const char* arg2_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    if (arg2 != Qnil) {
+        arg2_s = STR2CSTR(arg2);
+    }
+    do_wall(player_ref, arg1_s, arg2_s);
+    return Qnil;
+}
+
+static VALUE do_do_gripe(VALUE self, VALUE player, VALUE arg1, VALUE arg2)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    const char* arg1_s = 0;
+    const char* arg2_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    if (arg2 != Qnil) {
+        arg2_s = STR2CSTR(arg2);
+    }
+    do_gripe(player_ref, arg1_s, arg2_s);
+    return Qnil;
+}
+
+static VALUE do_do_page(VALUE self, VALUE player, VALUE arg1)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    const char* arg1_s = 0;
+    if (arg1 != Qnil) {
+        arg1_s = STR2CSTR(arg1);
+    }
+    do_page(player_ref, arg1_s);
+    return Qnil;
+}
+
+static VALUE do_notify_except(VALUE self, VALUE first, VALUE exception, VALUE msg)
+{
+    (void) self;
+    dbref first_ref = FIX2INT(first);
+    dbref exception_ref = FIX2INT(exception);
+    const char* msg_s = 0;
+    if (msg != Qnil) {
+        msg_s = STR2CSTR(msg);
+    }
+    notify_except(first_ref, exception_ref, msg_s);
+    return Qnil;
+}
+
 void Init_speech()
 {   
     speech_class = rb_define_class_under(tinymud_module, "Speech", rb_cObject);
+    rb_define_method(speech_class, "reconstruct_message", do_reconstruct_message, 2);
+    rb_define_method(speech_class, "do_say", do_do_say, 3);
+    rb_define_method(speech_class, "do_pose", do_do_pose, 3);
+    rb_define_method(speech_class, "do_wall", do_do_wall, 3);
+    rb_define_method(speech_class, "do_gripe", do_do_gripe, 3);
+    rb_define_method(speech_class, "do_page", do_do_page, 2);
+    rb_define_method(speech_class, "notify_except", do_notify_except, 3);
 }
--- ruby/test/speech.rb
+++ ruby/test/speech.rb
@@ -15,9 +15,188 @@ module TinyMud
 			@db.free()
 		end
 		
-		def test_new
+		def test_reconstruct_message
 			speech = Speech.new
-			assert_not_nil(speech)
+			assert_equal("hello", speech.reconstruct_message("hello", nil))
+			assert_equal("hello = world", speech.reconstruct_message("hello", "world"))
+		end
+		
+		def test_do_say
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			joe = Player.new.create_player("joe", "pod")
+			speech = Speech.new
+			
+			# If the player is nowhere then nothing is heard
+			Interface.expects(:do_notify).never
+			record(bob) {|r| r[:location] = NOTHING }
+			speech.do_say(bob, "hello", "world")
+			
+			# If the player is somewhere
+			notify = sequence('notify')
+			Interface.expects(:do_notify).with(bob, "You say \"hello = world\"").in_sequence(notify)
+			Interface.expects(:do_notify).with(joe, "bob says \"hello = world\"").in_sequence(notify)
+			Interface.expects(:do_notify).with(wizard, "bob says \"hello = world\"").in_sequence(notify)
+			record(bob) {|r| r[:location] = 0 }
+			speech.do_say(bob, "hello", "world")
+		end
+		
+		def test_do_pose
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			joe = Player.new.create_player("joe", "pod")
+			speech = Speech.new
+
+			# If the player is nowhere then nothing is heard
+			Interface.expects(:do_notify).never
+			record(bob) {|r| r[:location] = NOTHING }
+			speech.do_pose(bob, "hello", "world")
+			
+			# If the player is somewhere
+			notify = sequence('notify')
+			Interface.expects(:do_notify).with(joe, "bob hello = world").in_sequence(notify)
+			Interface.expects(:do_notify).with(bob, "bob hello = world").in_sequence(notify)
+			Interface.expects(:do_notify).with(wizard, "bob hello = world").in_sequence(notify)
+			record(bob) {|r| r[:location] = 0 }
+			speech.do_pose(bob, "hello", "world")
+		end
+		
+		def test_do_wall
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			joe = Player.new.create_player("joe", "pod")
+			speech = Speech.new
+			
+			# Normal player
+			Interface.expects(:do_notify).with(joe, 'But what do you want to do with the wall?')
+			speech.do_wall(joe, "hello", "world")
+			
+			# Wizard
+			notify = sequence('notify')
+			Interface.expects(:do_notify).with(wizard, "Wizard shouts \"hello = world\"").in_sequence(notify)
+			Interface.expects(:do_notify).with(bob, "Wizard shouts \"hello = world\"").in_sequence(notify)
+			Interface.expects(:do_notify).with(joe, "Wizard shouts \"hello = world\"").in_sequence(notify)
+			speech.do_wall(wizard, "hello", "world")
+			# Fixme: write stderr to somewhere else
+		end
+
+		def test_do_gripe
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			record(bob) {|r| r[:location] = 0 }
+			Interface.expects(:do_notify).with(bob, 'Your complaint has been duly noted.')
+			Speech.new.do_gripe(bob, "darn trolls", "eat cheese")
+			# Fixme: write stderr to somewhere else
+		end
+
+		def test_do_page
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			joe = Player.new.create_player("joe", "pod")
+
+			speech = Speech.new
+			record(bob) {|r| r[:pennies] = 0 }
+			Interface.expects(:do_notify).with(bob, "You don't have enough pennies.")
+			speech.do_page(bob, "joe")
+			
+			record(bob) {|r| r[:pennies] = LOOKUP_COST }
+			Interface.expects(:do_notify).with(bob, "I don't recognize that name.")
+			speech.do_page(bob, "jed")
+			
+			record(bob) {|r| r[:pennies] = LOOKUP_COST }
+			Interface.expects(:do_notify).with(joe, "You sense that bob is looking for you in Limbo.")
+			Interface.expects(:do_notify).with(bob, "Your message has been sent.")
+			speech.do_page(bob, "joe")
+		end
+
+		def test_notify_except
+			Db.Minimal()
+			wizard = 1
+			bob = Player.new.create_player("bob", "pwd")
+			joe = Player.new.create_player("joe", "pod")
+			
+			# Not sure if you chain people like this but its only testing the "next" chain on an object
+			record(wizard) {|r| r[:next] = bob }
+			record(bob) {|r| r[:next] = joe }
+			record(joe) {|r| r[:next] = NOTHING }
+			
+			speech = Speech.new
+			Interface.expects(:do_notify).with(wizard, "foo")
+			Interface.expects(:do_notify).with(bob, "foo")
+			speech.notify_except(wizard, joe, "foo")
+			
+			Interface.expects(:do_notify).never
+			Interface.expects(:do_notify).with(joe, "foo")
+			Interface.expects(:do_notify).with(bob, "foo")
+			speech.notify_except(wizard, wizard, "foo")
+		end
+
+		# MOVE THIS SOMEWHERE - DRY
+		def record(i)
+			record = @db.get(i)
+
+			args = {}
+			args[:name] = record.name
+			args[:description] = record.description
+			args[:location] = record.location
+			args[:contents] = record.contents
+			args[:exits] = record.exits
+			args[:next] = record.next
+			args[:key] = record.key
+			args[:fail] = record.fail
+			args[:succ] = record.succ
+			args[:ofail] = record.ofail
+			args[:osucc] = record.osucc
+			args[:owner] = record.owner
+			args[:pennies] = record.pennies
+			args[:flags] = record.flags
+			args[:password] = record.password
+
+			yield args
+
+			args.each do |key, value|
+				case key
+				when :name
+					record.name = value
+				when :description
+					record.description = value
+				when :location
+					record.location = value
+				when :contents
+					record.contents = value
+				when :exits
+					record.exits = value
+				when :next
+					record.next = value
+				when :key
+					record.key = value
+				when :fail
+					record.fail = value
+				when :succ
+					record.succ = value
+				when :ofail
+					record.ofail = value
+				when :osucc
+					record.osucc = value
+				when :owner
+					record.owner = value
+				when :pennies
+					record.pennies = value
+				when :flags
+					record.flags = value
+				when :password
+					record.password = value
+				else
+					raise("Record - unknown key #{key} with #{value}")
+				end
+			end
+
+			@db.put(i, record)
 		end
     end
 end
