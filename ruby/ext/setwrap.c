cf8820027d8ca8e6e8dd7421b2f3dad3a246972f
mangled <matthew@mangled.me>
2010-12-22 21:28:20 +0000

    testing set

--- ruby/ext/setwrap.c
+++ ruby/ext/setwrap.c
@@ -5,7 +5,186 @@
 
 static VALUE set_class;
 
+static VALUE do_do_name(VALUE self, VALUE player, VALUE name, VALUE newname)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* newname_s = strdup("\0");
+    if (newname != Qnil) {
+        newname_s = STR2CSTR(newname);
+    }
+    do_name(player_ref, name_s, newname_s);
+    return Qnil;
+}
+
+static VALUE do_do_describe(VALUE self, VALUE player, VALUE name, VALUE description)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* description_s = strdup("\0");
+    if (description != Qnil) {
+        description_s = STR2CSTR(description);
+    }
+    do_describe(player_ref, name_s, description_s);
+    return Qnil;
+}
+
+static VALUE do_do_fail(VALUE self, VALUE player, VALUE name, VALUE message)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* message_s = strdup("\0");
+    if (message != Qnil) {
+        message_s = STR2CSTR(message);
+    }
+    do_fail(player_ref, name_s, message_s);
+    return Qnil;
+}
+
+static VALUE do_do_success(VALUE self, VALUE player, VALUE name, VALUE message)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* message_s = strdup("\0");
+    if (message != Qnil) {
+        message_s = STR2CSTR(message);
+    }
+    do_success(player_ref, name_s, message_s);
+    return Qnil;
+}
+
+static VALUE do_do_osuccess(VALUE self, VALUE player, VALUE name, VALUE message)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* message_s = strdup("\0");
+    if (message != Qnil) {
+        message_s = STR2CSTR(message);
+    }
+    do_osuccess(player_ref, name_s, message_s);
+    return Qnil;
+}
+
+static VALUE do_do_ofail(VALUE self, VALUE player, VALUE name, VALUE message)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* message_s = strdup("\0");
+    if (message != Qnil) {
+        message_s = STR2CSTR(message);
+    }
+    do_ofail(player_ref, name_s, message_s);
+    return Qnil;
+}
+
+static VALUE do_do_lock(VALUE self, VALUE player, VALUE name, VALUE keyname)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* keyname_s = strdup("\0");
+    if (keyname != Qnil) {
+        keyname_s = STR2CSTR(keyname);
+    }
+    do_lock(player_ref, name_s, keyname_s);
+    return Qnil;
+}
+
+static VALUE do_do_unlock(VALUE self, VALUE player, VALUE name)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    do_unlock(player_ref, name_s);
+    return Qnil;
+}
+
+static VALUE do_do_unlink(VALUE self, VALUE player, VALUE name)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    do_unlink(player_ref, name_s);
+    return Qnil;
+}
+
+static VALUE do_do_chown(VALUE self, VALUE player, VALUE name, VALUE newobj)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* newobj_s = strdup("\0");
+    if (newobj != Qnil) {
+        newobj_s = STR2CSTR(newobj);
+    }
+    do_chown(player_ref, name_s, newobj_s);
+    return Qnil;
+}
+
+static VALUE do_do_set(VALUE self, VALUE player, VALUE name, VALUE flag)
+{
+    (void) self;
+    dbref player_ref = FIX2INT(player);
+    char* name_s = strdup("\0");
+    if (name != Qnil) {
+        name_s = STR2CSTR(name);
+    }
+    char* flag_s = strdup("\0");
+    if (flag != Qnil) {
+        flag_s = STR2CSTR(flag);
+    }
+    do_set(player_ref, name_s, flag_s);
+    return Qnil;
+}
+
 void Init_set()
 {   
     set_class = rb_define_class_under(tinymud_module, "Set", rb_cObject);
+    rb_define_method(set_class, "do_name", do_do_name, 3);
+    rb_define_method(set_class, "do_describe", do_do_describe, 3);
+    rb_define_method(set_class, "do_fail", do_do_fail, 3);
+    rb_define_method(set_class, "do_success", do_do_success, 3);
+    rb_define_method(set_class, "do_osuccess", do_do_osuccess, 3);
+    rb_define_method(set_class, "do_ofail", do_do_ofail, 3);
+    rb_define_method(set_class, "do_lock", do_do_lock, 3);
+    rb_define_method(set_class, "do_unlock", do_do_unlock, 2);
+    rb_define_method(set_class, "do_unlink", do_do_unlink, 2);
+    rb_define_method(set_class, "do_chown", do_do_chown, 3);
+    rb_define_method(set_class, "do_set", do_do_set, 3);
 }
--- ruby/test/set.rb
+++ ruby/test/set.rb
@@ -15,8 +15,378 @@ module TinyMud
 			@db.free()
 		end
 		
-		def test_something
-			# Todo
+		def test_do_name
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			fish = @db.add_new_record
+			record(place) {|r| r.merge!({ :name => "place", :description => "yellow", :succ=>"yippee", :fail => "shucks", :osucc => "ping", :ofail => "darn", :contents => bob, :flags => TYPE_ROOM }) }
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => fish ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			record(fish) {|r| r.merge!({ :name => "fish", :location => place, :description => "slimy", :flags => TYPE_THING, :owner => anne  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+			
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_name(bob, "doesn't exist", "become real")
+			
+			# Player must control the "thing" to name
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_name(bob, "fish", "chip")
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_name(bob, "here", "haddock")
+			
+			# Rename self!
+			# Missing password
+			Interface.expects(:do_notify).with(bob, "Give it what new name?").in_sequence(notify)
+			set.do_name(bob, "bob", nil)
+			# Empty password
+			Interface.expects(:do_notify).with(bob, "You must specify a password to change a player name.").in_sequence(notify)
+			Interface.expects(:do_notify).with(bob, "E.g.: name player = newname password").in_sequence(notify)
+			set.do_name(bob, "bob", "mary ")
+			# Incorrect password
+			Interface.expects(:do_notify).with(bob, "Incorrect password.").in_sequence(notify)
+			set.do_name(bob, "bob", "mary sprouts")
+			# Not got enough money
+			Interface.expects(:do_notify).with(bob, "You can't give a player that name.").in_sequence(notify)
+			set.do_name(bob, "bob", "mary sprout")
+			# Enough money, bad name
+			record(bob) {|r| r[:pennies] = LOOKUP_COST}
+			Interface.expects(:do_notify).with(bob, "You can't give a player that name.").in_sequence(notify) # Same as above!
+			set.do_name(bob, "bob", "here sprout")
+			assert_equal(0, @db.get(bob).pennies) # But it has taken your money - Bug!
+			# All ok
+			record(bob) {|r| r[:pennies] = LOOKUP_COST}
+			Interface.expects(:do_notify).with(bob, "Name set.").in_sequence(notify)
+			set.do_name(bob, "bob", "mary sprout")
+			assert_equal("mary", @db.get(bob).name)
+			
+			# Rename a non-player (thing) you own (note code checks all the same so won't repeat)
+			# Poor name
+			Interface.expects(:do_notify).with(bob, "That is not a reasonable name.").in_sequence(notify)
+			set.do_name(bob, "cheese", "me")
+			# Ok
+			Interface.expects(:do_notify).with(bob, "Name set.").in_sequence(notify)
+			set.do_name(bob, "cheese", "pie")
+			assert_equal("pie", @db.get(cheese).name)
+		end
+		
+		# The next few tests could/should be common'd up (they only differ by field tested)
+		def test_do_describe
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_describe(bob, "doesn't exist", "become real")
+			
+			# Can change description so long as you own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_describe(bob, "anne", "fred")
+			
+			# Something we own
+			Interface.expects(:do_notify).with(bob, "Description set.").in_sequence(notify)
+			set.do_describe(bob, "cheese", "best eaten early in the day")
+			assert_equal("best eaten early in the day", @db.get(cheese).description)
+		end
+		
+		def test_do_fail
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_fail(bob, "doesn't exist", "become real")
+			
+			# Can change description so long as you own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_fail(bob, "anne", "fred")
+			
+			# Something we own
+			Interface.expects(:do_notify).with(bob, "Message set.").in_sequence(notify)
+			set.do_fail(bob, "cheese", "you failed to eat the cheese")
+			assert_equal("you failed to eat the cheese", @db.get(cheese).fail)
+		end
+		
+		def test_do_success
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_success(bob, "doesn't exist", "become real")
+			
+			# Can change description so long as you own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_success(bob, "anne", "fred")
+			
+			# Something we own
+			Interface.expects(:do_notify).with(bob, "Message set.").in_sequence(notify)
+			set.do_success(bob, "cheese", "you eat the cheese")
+			assert_equal("you eat the cheese", @db.get(cheese).succ)
+		end
+		
+		def test_do_osuccess
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_osuccess(bob, "doesn't exist", "become real")
+			
+			# Can change description so long as you own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_osuccess(bob, "anne", "fred")
+			
+			# Something we own
+			Interface.expects(:do_notify).with(bob, "Message set.").in_sequence(notify)
+			set.do_osuccess(bob, "cheese", "bob eat the cheese")
+			assert_equal("bob eat the cheese", @db.get(cheese).osucc)
+		end
+		
+		def test_do_ofail
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_ofail(bob, "doesn't exist", "become real")
+			
+			# Can change description so long as you own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_ofail(bob, "anne", "fred")
+			
+			# Something we own
+			Interface.expects(:do_notify).with(bob, "Message set.").in_sequence(notify)
+			set.do_ofail(bob, "cheese", "bob failed to eat the cheese")
+			assert_equal("bob failed to eat the cheese", @db.get(cheese).ofail)			
+		end
+
+		def test_do_lock
+			# I can't work out *still* how to get ambigous matches!!! So far none of the tests cover this match case!!!
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			exit = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => exit  }) }
+			record(exit) {|r| r.merge!( :location => NOTHING, :name => "exit", :flags => TYPE_EXIT, :owner => bob, :next => NOTHING ) }
+
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+			
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see what you want to lock!").in_sequence(notify)
+			set.do_lock(bob, "spaghetti", "sauce")
+			
+			# Don't control
+			Interface.expects(:do_notify).with(bob, "You can't lock that!").in_sequence(notify)
+			set.do_lock(bob, "anne", "sauce")
+			
+			# Ok, now onto the "key" - Doesn't exist!
+			Interface.expects(:do_notify).with(bob, "I can't find that key!").in_sequence(notify)
+			set.do_lock(bob, "cheese", "sauce")
+			
+			# Key is not a player or thing
+			Interface.expects(:do_notify).with(bob, "Keys can only be players or things.").in_sequence(notify)
+			set.do_lock(bob, "cheese", "exit")
+			
+			# Ok - Do it!
+			Interface.expects(:do_notify).with(bob, "Locked.").in_sequence(notify)
+			set.do_lock(bob, "cheese", "anne")
+			assert_equal(anne, @db.get(cheese).key)
+			assert_equal(TYPE_THING, @db.get(cheese).flags)
+			
+			# Now with antilock
+			record(cheese) {|r| r[:key] = NOTHING }
+			Interface.expects(:do_notify).with(bob, "Anti-Locked.").in_sequence(notify)
+			set.do_lock(bob, "cheese", "!anne")
+			assert_equal(anne, @db.get(cheese).key)
+			assert_equal(TYPE_THING | ANTILOCK, @db.get(cheese).flags)
+		end
+
+		def test_do_unlock
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+			
+			# Thing doesn't exist
+			Interface.expects(:do_notify).with(bob, "I don't see that here.").in_sequence(notify)
+			set.do_unlock(bob, "spaghetti")
+			
+			# Must own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_unlock(bob, "anne")
+			
+			# Do it (from normal lock)
+			Interface.expects(:do_notify).with(bob, "Locked.").in_sequence(notify)
+			set.do_lock(bob, "cheese", "anne")
+			Interface.expects(:do_notify).with(bob, "Unlocked.").in_sequence(notify)
+			set.do_unlock(bob, "cheese")
+			assert_equal(NOTHING, @db.get(cheese).key)
+			assert_equal(TYPE_THING, @db.get(cheese).flags)
+			
+			# Do it from antilock
+			Interface.expects(:do_notify).with(bob, "Anti-Locked.").in_sequence(notify)
+			set.do_lock(bob, "cheese", "!anne")
+			Interface.expects(:do_notify).with(bob, "Unlocked.").in_sequence(notify)
+			set.do_unlock(bob, "cheese")
+			assert_equal(NOTHING, @db.get(cheese).key)
+			assert_equal(TYPE_THING, @db.get(cheese).flags)
+		end
+
+		def test_do_unlink
+			# I can't work out *still* how to get ambigous matches!!! So far none of the tests cover this match case!!!
+			Db.Minimal()
+			limbo = 0
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			exit = @db.add_new_record
+			jam = @db.add_new_record
+			record(place) {|r| r.merge!({ :location => limbo, :name => "place", :contents => bob, :flags => TYPE_ROOM, :exits => exit }) }
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => NOTHING ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			record(jam) {|r| r.merge!({ :name => "jam", :location => place, :description => "red", :flags => TYPE_THING, :owner => bob, :next => NOTHING }) }
+			record(exit) {|r| r.merge!( :location => limbo, :name => "exit", :description => "long", :flags => TYPE_EXIT, :next => NOTHING ) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+			
+			# Thing doesn't exist or is a player
+			Interface.expects(:do_notify).with(bob, "Unlink what?").in_sequence(notify)
+			set.do_unlink(bob, "spaghetti")
+			Interface.expects(:do_notify).with(bob, "Unlink what?").in_sequence(notify)
+			set.do_unlink(bob, "anne")
+			
+			# Must own
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_unlink(bob, "exit")
+			
+			# Must be an exit or room
+			# But I think only a wizard can hit this logic as the normal match code won't pick up things!
+			
+			# Do it!
+			record(exit) {|r| r[:owner] = bob }
+			Interface.expects(:do_notify).with(bob, "Unlinked.").in_sequence(notify)
+			set.do_unlink(bob, "exit")
+			assert_equal(NOTHING, @db.get(exit).location)
+			
+			# Do it on a room
+			record(place) {|r| r[:owner] = bob }
+			Interface.expects(:do_notify).with(bob, "Dropto removed.").in_sequence(notify)
+			set.do_unlink(bob, "here")
+			assert_equal(NOTHING, @db.get(place).location)
+			
+			# Remember wizard absolute - Try it on a thing
+			record(bob) {|r| r[:flags] = TYPE_PLAYER | WIZARD }
+			Interface.expects(:do_notify).with(bob, "You can't unlink that!").in_sequence(notify)
+			set.do_unlink(bob, "##{jam}")
+			
+			# They also have power!
+			record(exit) {|r| r[:owner] = anne }
+			record(exit) {|r| r[:location] = anne }
+			Interface.expects(:do_notify).with(bob, "Unlinked.").in_sequence(notify)
+			set.do_unlink(bob, "exit")
+			assert_equal(NOTHING, @db.get(exit).location)
+		end
+		
+		def test_do_chown
+			place = @db.add_new_record
+			bob = Player.new.create_player("bob", "sprout")
+			anne = Player.new.create_player("anne", "treacle")
+			cheese = @db.add_new_record
+			exit = @db.add_new_record
+			jam = @db.add_new_record
+			record(place) {|r| r.merge!({ :name => "place", :contents => bob, :flags => TYPE_ROOM, :exits => exit }) }
+			record(bob) {|r| r.merge!( :contents => cheese, :location => place, :next => anne ) }
+			record(anne) {|r| r.merge!( :contents => NOTHING, :location => place, :next => jam ) }
+			record(cheese) {|r| r.merge!({ :name => "cheese", :location => bob, :description => "wiffy", :flags => TYPE_THING, :owner => bob, :next => NOTHING  }) }
+			record(jam) {|r| r.merge!({ :name => "jam", :location => place, :description => "red", :flags => TYPE_THING, :owner => bob, :next => NOTHING }) }
+			record(exit) {|r| r.merge!( :name => "exit", :description => "long", :flags => TYPE_EXIT, :owner => anne, :next => NOTHING ) }
+			
+			set = TinyMud::Set.new
+			notify = sequence('notify')
+			
+			# Person must be a wizard
+			Interface.expects(:do_notify).with(bob, "Permission denied.").in_sequence(notify)
+			set.do_chown(bob, "jam", "anne")
+			
+			# Missing owning object
+			record(bob) {|r| r[:flags] = TYPE_PLAYER | WIZARD }
+			Interface.expects(:do_notify).with(bob, "I couldn't find that player.").in_sequence(notify)
+			set.do_chown(bob, "jam", "twig")
+			
+			# Chown on a player
+			Interface.expects(:do_notify).with(bob, "Players always own themselves.").in_sequence(notify)
+			set.do_chown(bob, "anne", "bob")
+			
+			# Ok!
+			Interface.expects(:do_notify).with(bob, "Owner changed.").in_sequence(notify)
+			set.do_chown(bob, "cheese", "anne")
+			assert_equal(anne, @db.get(cheese).owner)
+			Interface.expects(:do_notify).with(bob, "Owner changed.").in_sequence(notify)
+			set.do_chown(bob, "here", "anne")
+			assert_equal(anne, @db.get(place).owner)
+		end
+		
+		def test_do_set
+			# TODO!!! Brain ran out of steam tonight!!!
 		end
 
 		# MOVE THIS SOMEWHERE - DRY
