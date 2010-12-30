3a744bc41034b10e61d705f66ab81c4c97492e77
mangled <matthew@mangled.me>
2010-12-30 15:20:38 +0000

    finished testing stringutil

--- ruby/ext/stringutilwrap.c
+++ ruby/ext/stringutilwrap.c
@@ -5,7 +5,52 @@
 
 static VALUE stringutil_class;
 
+static VALUE do_string_compare(VALUE self, VALUE s1, VALUE s2)
+{
+    (void) self;
+    char* s1_s = strdup("\0");
+    if (s1 != Qnil) {
+        s1_s = STR2CSTR(s1);
+    }
+    char* s2_s = strdup("\0");
+    if (s2 != Qnil) {
+        s2_s = STR2CSTR(s2);
+    }
+    return INT2FIX(string_compare(s1_s, s2_s));
+}
+
+static VALUE do_string_prefix(VALUE self, VALUE string, VALUE prefix)
+{
+    (void) self;
+    char* string_s = strdup("\0");
+    if (string != Qnil) {
+        string_s = STR2CSTR(string);
+    }
+    char* prefix_s = strdup("\0");
+    if (prefix != Qnil) {
+        prefix_s = STR2CSTR(prefix);
+    }
+    return INT2FIX(string_prefix(string_s, prefix_s));
+}
+
+static VALUE do_string_match(VALUE self, VALUE src, VALUE sub)
+{
+    (void) self;
+    char* src_s = strdup("\0");
+    if (src != Qnil) {
+        src_s = STR2CSTR(src);
+    }
+    char* sub_s = strdup("\0");
+    if (sub != Qnil) {
+        sub_s = STR2CSTR(sub);
+    }
+    return INT2FIX(string_match(src_s, sub_s));
+}
+
 void Init_stringutil()
 {   
     stringutil_class = rb_define_class_under(tinymud_module, "StringUtil", rb_cObject);
+    rb_define_method(stringutil_class, "string_compare", do_string_compare, 2);
+    rb_define_method(stringutil_class, "string_prefix", do_string_prefix, 2);
+    rb_define_method(stringutil_class, "string_match", do_string_match, 2);
 }
--- ruby/test/stringutil.rb
+++ ruby/test/stringutil.rb
@@ -5,19 +5,43 @@ require 'defines'
 require 'tinymud'
 require 'pp'
 
+# Only testing these as they may be non typical comparison routines
 module TinyMud
     class TestStringUtil < Test::Unit::TestCase
-		def setup
-			@db = TinyMud::Db.new
-		end
-
-		def test_something
+		def test_string_compare # Zero implies equal - Basically a case insensitive equals
 			su = TinyMud::StringUtil.new
-			assert_not_nil(su)
+			assert_equal(0, su.string_compare(nil, nil))
+			assert_not_equal(0, su.string_compare("a", nil))
+			assert_not_equal(0, su.string_compare(nil, "a"))
+			assert_equal(0, su.string_compare("a", "a"))
+			assert_equal(0, su.string_compare("A", "a"))
+			assert_equal(0, su.string_compare("a", "A"))
+			assert_equal(0, su.string_compare("abcdef", "abcDeF"))
+			assert_not_equal(0, su.string_compare("abcdex", "abcDeF"))
 		end
 
-		def teardown
-			@db.free()
+		def test_string_prefix # Check (case insensitive) that the string has the prefix
+			# Returns true (1) or false (0)
+			su = TinyMud::StringUtil.new
+			assert_equal(1, su.string_prefix(nil, nil))
+			assert_equal(0, su.string_prefix("foo", "bar"))
+			assert_equal(0, su.string_prefix("", "bar"))
+			assert_equal(1, su.string_prefix("barfoo", "bar"))
+			assert_equal(1, su.string_prefix("bArfoo", "bar"))
+			assert_equal(1, su.string_prefix("bAR foo", "bar"))
+			assert_equal(0, su.string_prefix(" bAR foo", "bar"))
+		end
+		
+		def test_string_match # Is a substring of s, match on start of word, if so return address else zero
+			su = TinyMud::StringUtil.new
+			assert_equal(0, su.string_match(nil, nil))
+			assert_equal(0, su.string_match("hello world", "foo"))
+			assert_equal(0, su.string_match("hello world", "ell"))
+			assert_not_equal(0, su.string_match("hello world", "hel"))
+			assert_not_equal(0, su.string_match("hello world", "wor"))
+			assert_not_equal(0, su.string_match("hello world", "WORLD"))
+			assert_not_equal(0, su.string_match("hello big world", "WORLD"))
+			assert_equal(0, su.string_match("hello big world", "ORLD"))
 		end
     end
 end
