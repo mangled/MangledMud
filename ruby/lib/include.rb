51ed4d5f51726ecf3c891eb5c2a5f692a43e3bd5
Alexander Morrow <amo3@umbc.edu>
2012-01-25 22:18:15 -0500

    Ruby conversion of db.c mostly done - Some things to clean up still to make tests pass. Include.rb not changed - only move so require_relative path would be simple - This change can be discarded when db.rb is added to master. -amo3

---
+++ ruby/lib/include.rb
@@ -0,0 +1,14 @@
+# Switch the extension library that is used in the test.
+
+if ENV['TEST_TYPE'] == 'ORIGINAL' # The original
+  puts "Fyi - You are running against the ORIGINAL (c-extensions)"
+  require_relative '../lib/original/tinymud'
+elsif ENV['TEST_TYPE'] == 'CONVERTED' # The diminishing library
+  puts "Fyi - You are running against the CONVERTED (being converted library)"
+  require_relative '../lib/converted/tinymud'
+  require_relative '../lib/db.rb'
+  # To-do - require converted ruby class (new folder?) (and disable in tinymud.c)
+else
+  throw "Unknown test type!"
+end
+
