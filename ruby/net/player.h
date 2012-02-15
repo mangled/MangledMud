bcdc00892fc5ccb416792e33a9b1b37d1580670c
mangled <matthew@mangled.me>
2012-02-15 18:35:47 +0000

    #40 cleaning up

--- ruby/net/player.h
+++
@@ -1,6 +0,0 @@
-/* Prototypes for externs not defined elsewhere */
-#include "db.h"
-
-/* From player.c */
-extern dbref lookup_player(const char *name);
-extern void do_password(dbref player, const char *old, const char *newobj);
