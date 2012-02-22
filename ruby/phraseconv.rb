e38a7a9ca7f17d04c16fb1c7b992763c7a667a35
mangled <matthew@mangled.me>
2012-02-22 21:03:59 +0000

    preparing to run tool

--- ruby/phraseconv.rb
+++ ruby/phraseconv.rb
@@ -2,6 +2,7 @@
 # this may turn out harder than doing it by hand
 require "find"
 require 'yaml'
+require 'fileutils'
 require 'pp'
 
 # load the phrases dictionary
@@ -22,7 +23,10 @@ files.each do |filename|
   puts "Parsing #{filename}"
   match_notify = /(\s*Interface\.do_notify\(\w+,)\s*(["']{1}.*?)\)(.*)/
   match_expects = /(\s*Interface\.expects\(:do_notify\)\.with\([\w'"]+,)\s*(["']{1}.*?)\)(.*)/
-  open(filename + ".tmp", "w+") do |out|
+  tmp_name = filename.gsub(".rb", ".tmp")
+  #FileUtils.cp(filename, new_name)
+  puts "#{filename} to #{tmp_name}"
+  open(tmp_name, "w+") do |out|
     IO.foreach(filename) do |line|
       m1 = line.match(match_notify)
       m2 = line.match(match_expects)
@@ -32,6 +36,9 @@ files.each do |filename|
           #raise "poo2" if m1[3].strip.length != 0
           puts "matched #{m1[2]} with \"#{found[1]}\"key: #{found[0]}"
           out.puts(m1[1] + " Phrasebook.lookup('#{found[0]}')" + ")" + m1[3].strip)
+        else
+          puts "Note: found a match but no key for #{m1[2]}"
+          out.puts(line.chomp())
         end
       elsif m2
         found = phrases.find {|k, v| v == m2[2][1..-2] }
@@ -39,6 +46,9 @@ files.each do |filename|
           #raise "poo2 #{m2[3]}" if m2[3].strip.length != 0
           puts "matched #{m2[2]} with \"#{found[1]}\" key: #{found[0]}"
           out.puts(m2[1] + " Phrasebook.lookup('#{found[0]}')" + ")" + m2[3].strip)
+        else
+          puts "Note: found a match but no key for #{m2[2]}"
+          out.puts(line.chomp())
         end
       else
         out.puts(line.chomp())
