7e81e4e517f4488a87438460adb79cf31d7adace
mangled <matthew@mangled.me>
2012-02-25 21:59:17 +0000

    fixed up help, we will need to review this doc. at some point, but at least we can see how to get it to pass

--- ruby/test/help_test.rb
+++ ruby/test/help_test.rb
@@ -21,6 +21,7 @@ module TinyMud
 			notify = sequence('notify')
 			@notifier.expects(:do_notify).with(0, "This is TinyMUD version 1.3, a user-extendible, multi-user adventure game.").in_sequence(notify)
 			@notifier.expects(:do_notify).with(0, "Basic commands: ").in_sequence(notify)
+			@notifier.expects(:do_notify).with(0, anything()).times(21).in_sequence(notify)
 			help.do_help(0)
 		end
 
